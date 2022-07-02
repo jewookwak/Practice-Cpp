7a17551e2543ff58eeefc5d23f0911a305543492
jewookwak <jewoo2963@naver.com>
2022-07-02 16:13:50 +0900

    test_week06_1

--- week06/week06/test_week06_1.cpp
+++ week06/week06/test_week06_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 // OOP Overview and Features
 
-int main() {
 
-	return 0;
+class Box {
+public:
+	Box(double h, double w, double l) : height(h), width(w), length(l) {}
+	double volume() { return height * width * length; }
+	void print() {
+		cout << height << " " << width << " " << length << endl;
+	}
+private:
+	double height, width, length;
+};
+
+void main() {
+	Box b(10, 20, 30);
+	b.print();
+	cout << b.volume() << endl;
 }
