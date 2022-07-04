f86fc05c72797f9904b42c07a6b55b3a5b170856
jewookwak <jewoo2963@naver.com>
2022-07-04 17:50:29 +0900

    test_week06_07

---
+++ week06/week06/test_week06_07.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Box {
+public:
+	Box() {
+		height = width = length = 0;
+		cout << "Constructor 0" << endl;
+	}
+	Box(double h, double w, double l) {
+		height = h; width = w; length = l;
+		cout << "Constructor 1" << endl;
+	}
+	double height, width, length;
+};
+
+void main() {
+	Box box1;
+	Box box2(1, 1, 1);
+}
