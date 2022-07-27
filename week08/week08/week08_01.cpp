c0a5ddc700c359044acc1ad0fed0fc4355c4ce39
jewookwak <jewoo2963@naver.com>
2022-07-27 13:52:47 +0900

    Example of IS-A Relationship

--- week08/week08/week08_01.cpp
+++ week08/week08/week08_01.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 //inheritance
+//Example of IS-A Relationship
+class Person
+{
+private:
+	int age;
+	char name[50];
+public:
+	Person(int myage, const char* myname) : age(myage)
+	{
+		strcpy_s(name, myname);
+	}
+	void WhatYourName() const
+	{
+		cout << "My Name is " << name << endl;
+	}
+	void HowOldAreYou() const
+	{
+		cout << "I am " << age << " years old" << endl;
+	}
+};
+class UnivStudent : public Person
+{
+private:
+	char major[50];
+public:
+	UnivStudent(const char* myname, int myage, const char* mymajor) : Person(myage, myname)
+	{
+		strcpy_s(major, mymajor);
+	}
+	void WhoAreYou() const
+	{
+		WhatYourName();
+		HowOldAreYou();
+		cout << "my Major is " << major << endl << endl;
+	}
+};
 int main()
 {
+	UnivStudent ustd1("Lee", 22, "Computer eng.");
+	ustd1.WhoAreYou();
+	UnivStudent ustd2("Yoon", 21, "Electronic eng");
+	ustd2.WhoAreYou();
 	return 0;
 }
