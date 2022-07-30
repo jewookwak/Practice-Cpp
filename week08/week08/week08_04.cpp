af6dd13532d510efb59aad67dec617c0d54e5961
jewookwak <jewoo2963@naver.com>
2022-07-31 00:04:10 +0900

    Flow of Destructor

--- week08/week08/week08_04.cpp
+++ week08/week08/week08_04.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 //Flow of Destructor
 //Destructor exexuted when main is over. Dectructor's order is reverse order of Constructor.
+/*
+When object of derived class is destroyed, destructor of the derived class is executed, then, destructor of the base class is executed.
+Order of destruction made in stack is the reverse of the order of creation.
+*/
 class Base
 {
 private:
