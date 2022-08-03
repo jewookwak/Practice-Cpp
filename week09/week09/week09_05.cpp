284be456f0ddedaf19726eaf543937ebb270f126
jewookwak <jewoo2963@naver.com>
2022-08-03 11:03:38 +0900

    An example of function overriding

---
+++ week09/week09/week09_05.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+// An example of function overriding
+
+
+class First
+{
+public:
+	void FirstFunc() { cout << "FirstFunc" << endl; }
+
+};
+class Second : public First
+{
+public:
+	void SecondFunc() { cout << "SecondFunc" << endl; }
+
+};
+class Third : public Second
+{
+public:
+	void ThirdFunc() { cout << "ThirdFunc" << endl; }
+};
+
+int main()
+{
+	Third* tptr = new Third();
+	Second* sptr = tptr;
+	First* fptr = sptr;
+
+	tptr->FirstFunc();
+	tptr->SecondFunc();
+	tptr->ThirdFunc(); // Third inherited First, Second so Ok to compile SecondFunc(), ThirdFunc()
+	/*
+	sptr->FirstFunc();
+	sptr->SecondFunc();
+	sptr->ThirdFunc(); // Error!  // Compiler doesn't know sptr points to Third(). Compiler only consider pointer type when it is declared (Second *sptr = ~~)
+
+	fptr->FirstFunc();
+	fptr->SecondFunc(); // Error!
+	fptr->ThirdFunc(); // Error!
+	
+	
+	*/
+	delete tptr;
+	return 0;
+}
