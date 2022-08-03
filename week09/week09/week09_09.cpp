be934adad177b76acd17f1b5f10d7e8d206b10c6
jewookwak <jewoo2963@naver.com>
2022-08-03 14:36:08 +0900

    An Example of Abstract Class

---
+++ week09/week09/week09_09.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+// An Example of Abstract Class
+
+class Animal
+{
+public:
+	virtual ~Animal() {} // virtual destructor
+	virtual void Cry() = 0; // Pure virtual function
+};
+class Dog : public Animal
+{
+public:
+	virtual void Cry() { cout << "Dog Cry!" << endl; }
+};
+
+class Cat : public Animal
+{
+public:
+	virtual void Cry() { cout << "Cat Cry!" << endl; }
+};
+
+int main(void)
+{
+	Dog my_dog;
+	my_dog.Cry();
+	Cat my_cat;
+	my_cat.Cry();
+	return 0;
+}
