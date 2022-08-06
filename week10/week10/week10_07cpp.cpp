334f765fc7df30cd1f0e7af1610e87e01a6c40cc
jewookwak <jewoo2963@naver.com>
2022-08-06 19:00:54 +0900

    specialization of function template

---
+++ week10/week10/week10_07cpp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+// An Example of Template
+// specialization of function template
+
+template <typename T>
+T Max(T a, T b)
+{
+	cout << "simple max" << endl;
+	return a > b ? a : b;
+}
+
+template <>
+char* Max(char* a, char* b)
+{
+	cout << "char* Max<char*>(char* a, char* b)" << endl;
+	return strlen(a) > strlen(b) ? a : b;
+}
+int main()
+{
+	cout << Max(11, 15) << endl;
+	cout << Max('T', 'Q') << endl;
+	cout << Max(3.5, 7.5) << endl;
+	cout << Max("constant", "test1") << endl;
+
+	char str1[] = "character";
+	char str2[] = "test2";
+	cout << Max(str1, str2) << endl;
+	return 0;
+}
