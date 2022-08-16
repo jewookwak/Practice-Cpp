6fae8db89db421052b65bcf227f3b45abc9aa08c
jewookwak <jewoo2963@naver.com>
2022-08-16 17:25:11 +0900

    Container Adapters: An Example of Queue

---
+++ week12/week12/week12_06.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+//Container Adapters: An Example of Queue
+void displayQ(queue <int> q1) {
+	for (int i = 0; i < 5; i++) {
+		cout << q1.front() << " ";
+		q1.pop();
+	}
+	cout << endl;
+}
+
+int main()
+{
+	queue <int> q1; queue <int> q2;
+	//inserting elements into the queue 1
+	for (int i = 0; i < 5; i++)
+	{
+		q1.push(i + 1);
+	}
+	cout << "elements 1,2,3,4,5 inseted into queue 1" << endl;
+	//inserting elements into the queue 2
+	for (int i = 0; i < 5; i++) {
+		q2.push(i * 10);
+	}
+
+	cout << "elements 0,10,20,30,40 inseted into queue 2" << endl;
+	q1.swap(q2);
+	cout << "contents after swapping operation" << endl;
+
+	cout << "elements of q1 are " < endl;
+	displayQ(q1);
+	cout << "elements of q2 are " << endl;
+	displayQ(q2);
+	return 0;
+}
